dfcc1a9b99466da3a1aeaa2929cda47e68b3e75e
GeorgEvseyev <georgevseyev@icloud.com>
2022-07-23 12:33:21 +0300

    Initial Commit

---
+++ ChallengeDay50/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeDay50
+//
+//  Created by Георгий Евсеев on 23.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
